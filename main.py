7c0df90a5b7813dc2e9e9d1836d10d51cc34ee96
Kevin Zhao <kzhao16@gmail.com>
2024-08-01 23:33:56 -0400

    Fix discount model + reformat/refactor

--- main.py
+++ main.py
@@ -55,12 +55,13 @@ def prediction():
     item_names = req.item_names
     retailer = req.retailer
 
-    ### query database
+    # Query the database to get the data that we need
+    query = "SELECT * FROM promos WHERE item_name IN :item_names \
+AND non_promo_price BETWEEN :min_price AND :max_price \
+AND retailer = :retailer;"
     with engine.begin() as conn:
         df = pd.read_sql(
-            sqlalchemy.text(
-                "SELECT * FROM promos WHERE item_name IN :item_names AND non_promo_price BETWEEN :min_price AND :max_price AND retailer = :retailer;"
-            ),
+            sqlalchemy.text(query),
             conn,
             params={
                 "item_names": tuple(item_names),
@@ -70,6 +71,8 @@ def prediction():
             },
             parse_dates=["retailer_week"],
         )
+
+    # TPR = Temporary Price Reduction
     df["tpr_disc"] = df["non_promo_price"] - df["promo_price"]
     df["is_offer"] = df["offer_message_0"].notnull().astype(int)
     if df.shape[0] == 0:
@@ -79,66 +82,45 @@ def prediction():
         )
         return jsonify(res.model_dump()), 400
 
-    # create calendar to help create merch grid
-    min_date = df["retailer_week"].min() - timedelta(
-        days=7
-        * ((df["retailer_week"].min() - (dt.now() - timedelta(days=7 * 104))).days // 7)
+    # Look at the minimum date in our data but synced with the cadence
+    # of the minimum and maximum promotion date
+    min_df_date = df["retailer_week"].min()
+    right_now = dt.now()
+    two_years_ago = right_now - timedelta(days=7 * 52 * 2)
+    one_year_from_now = right_now + timedelta(days=7 * 52)
+
+    min_date = min_df_date - timedelta(
+        days=((min_df_date - two_years_ago).days // 7) * 7
     )
-    max_date = df["retailer_week"].min() + timedelta(
-        days=7
-        * (((dt.now() + timedelta(days=7 * 52)) - df["retailer_week"].min()).days // 7)
+    max_date = min_df_date + timedelta(
+        days=((one_year_from_now - min_df_date).days // 7) * 7
     )
     calendar = pd.DataFrame(
-        columns=["retailer_week"], data=np.arange(min_date, max_date, timedelta(days=7))
+        columns=["retailer_week"],
+        data=np.arange(min_date, max_date, timedelta(days=7)),
     )
     calendar["week"] = calendar["retailer_week"].dt.isocalendar().week
     calendar["year"] = calendar["retailer_week"].dt.isocalendar().year
-    years = sorted(calendar["year"].unique())
 
-    ### create prediction for adblock
-
-    # create merch grid
+    # Create prediction for adblock
+    ## Create merch grid
     mechanic_col = "tpr_disc"
     merch_grid_raw = (
-        df.loc[(df["non_promo_price"].between(min_price, max_price))]
+        df.loc[df["non_promo_price"].between(min_price, max_price)]
         .groupby(by=["retailer_week"])[[mechanic_col]]
-        .first()
+        .median()
         .dropna()
     )
     merch_grid_raw = calendar.merge(merch_grid_raw, on="retailer_week", how="left")
     merch_grid_raw[mechanic_col] = merch_grid_raw[mechanic_col].fillna(0.0).round(2)
-    merch_grid = merch_grid_raw.pivot_table(
-        index="week",
-        columns="year",
-        values=mechanic_col,
-        aggfunc=lambda x: next(iter(x.dropna()), None),
-        dropna=False,
-    )
 
-    # add latest year's actual dates for reference
-    min_date_latest_year = calendar.loc[
-        (calendar["year"] == calendar["year"].max()), "retailer_week"
-    ].min()
-    latest_year_dates = pd.DataFrame(
-        index=np.arange(52),
-        data={
-            "retailer_week": np.arange(
-                min_date_latest_year,
-                min_date_latest_year + timedelta(days=7 * 52),
-                timedelta(days=7),
-            )
-        },
-    )
-    merch_grid = latest_year_dates.join(merch_grid)
-
-    ### create discounting model
-
-    # create lagged promo
+    # Create discount model
+    ## Create lagged promo
     merch_grid_raw[mechanic_col + "_ly"] = (
         merch_grid_raw[mechanic_col].shift(52).fillna(0.0)
     )
 
-    # create smoothed ly promo
+    # Create smoothed ly promo
     merch_grid_raw[mechanic_col + "_smoothed"] = (
         merch_grid_raw[mechanic_col + "_ly"]
         .rolling(window=3, center=True, win_type="gaussian", min_periods=1)
@@ -146,7 +128,7 @@ def prediction():
     )
     merch_grid_raw[mechanic_col + "_predicted"] = merch_grid_raw[
         mechanic_col + "_smoothed"
-    ].shift(52)
+    ]
 
     historical_week = merch_grid_raw.loc[
         (merch_grid_raw["retailer_week"] < dt.now()),
